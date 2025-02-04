80189c01f9c7bb174e004debbc4a11d6823ea358
Nishitha S Gandhi <nishitha.s@jmangroup.com>
2025-02-04 18:15:13 +0530

    Components added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import './App.css'
 import Header from './Components/Header'
 import Body from './Components/Body'
 import Footer from './Components/Footer'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './Components/Home'
+import Blog from './Components/Blog'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div><Header/></div>
-    <div><Body/></div>
-    <div><Footer/></div>
+    <BrowserRouter>
+    <Header/>
+      <Routes >
+        <Route path="/" element={<Home/>} />
+        <Route path="/Blog" element={<Blog/>}/>
+      </Routes>
+      <Footer/>
+        
+    </BrowserRouter>
+    
       
     </>
   )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { BookOpenText } from 'lucide-react'; 
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <header className="bg-gray-800 text-white shadow-md">
+      <div className="max-w-screen-xl mx-auto px-4 py-4 flex justify-between items-center">
+        <div className="flex items-center space-x-2">
+          <BookOpenText className="w-9 h-9 text-white" /> 
+          <h1 className="text-3xl font-semibold">Writely</h1>
+        </div>
+
+       {/*  <nav className="space-x-4">
+          <a href="/" className="text-lg hover:text-yellow-400 transition-all">Home</a>
+          <a href="/blogs" className="text-lg hover:text-yellow-400 transition-all">Blogs</a>
+          <a href="/about" className="text-lg hover:text-yellow-400 transition-all">About</a>
+          <a href="/contact" className="text-lg hover:text-yellow-400 transition-all">Contact</a>
+        </nav> */}
+        
+        {/* <div className="space-x-3">
+          <button className="bg-blue-500 px-6 py-2 rounded-lg text-white hover:bg-blue-600 transition-all">
+            Login
+          </button>
+          <button className="bg-darkblue-500 px-6 py-2 rounded-lg text-white hover:bg-green-600 transition-all">
+            Sign Up
+          </button>
+        </div> */}
+
+        <div>
+          <ul className='flex justify-between'>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Blog">Blog</Link></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
